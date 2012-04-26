2d2aefeeb9975b1cdb4c3710f65eb84103bcb754
fyears <hjwdhjwd@gmail.com>
2012-04-26 11:50:17 +0800

    fix go.sh again...

--- go.sh
+++ go.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-mv ./service/.smtp.php ./service/smtp.php
+cp -f ./service/.smtp.php ./service/smtp.php
+cp -f ./service/.dbconnect.php ./service/dbconnect.php
 find . -not -type d|xargs chmod 644
 find . -type d|xargs chmod 755
 chown -R www:www .
